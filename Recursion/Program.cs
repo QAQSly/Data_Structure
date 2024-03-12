ce676dd795081041be025ecabbbc9b34fcc8955d
QAQSly <2015672764@qq.com>
2024-03-12 11:37:22 +0800

    添加递归文件名方法

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace Recursion;
+﻿using System;
+using System.IO;
+namespace Recursion;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello, World!");
-        CountDown(10);
+        // CountDown(10);
+        //int temp = Factorial(3);
+        //Console.WriteLine(temp);
+        PrintDir("./");
     }
 
     static void CountDown(int num)
@@ -16,5 +21,32 @@ class Program
         }
         Console.WriteLine(num);
         CountDown(num - 1);
+    }
+
+    static int Factorial(int num)
+    {
+        if (num == 1)
+        {
+            return 1;
+        }
+
+        return num * Factorial(num -1);
+    }
+
+
+    static void PrintDir(string path)
+    {
+      string[] fileNames = Directory.GetDirectories(path,"*", SearchOption.TopDirectoryOnly);
+      if (fileNames.Length < 1)
+      {
+          return;
+      }
+
+      foreach(var name in fileNames)
+      {
+          Console.WriteLine(name);
+          PrintDir(name);
+      }
+
     }
 }
